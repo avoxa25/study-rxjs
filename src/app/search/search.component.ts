0d4251be4b8e3a99420461f7d2bdb4b834db395c
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-10-16 17:11:01 +0300

    Add logic to do search request from reddit

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  private searchSubject$ = new Subject<string>();
+  public results$?: Observable<any>;
+  public searchString: string = '';
+
 
   ngOnInit() {
+    this.results$ = this.searchSubject$
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        switchMap(value => this.queryApi(value))
+      );
+  }
+
+  public queryApi(searchString: string): Observable<any> {
+    console.log('queryApi', searchString);
+    return this.http.get(`https://www.reddit.com/r/battlemaps/search.json?q=${searchString}`)
+    .pipe(
+      map<any, any>(value => value['data']['children'])
+      )
+  }
+
+  public inputChanged($event: string): void {
+    console.log($event);
+    this.searchSubject$.next($event);
   }
 
 }
